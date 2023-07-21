860943fe426c2464c6c32016b9018db0822d439d
miox <gallantsummerwind@gmail.com>
2023-07-21 15:58:09 +0900

    headerのコンポーネントを追加

---
+++ src/components/HeaderComp.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderComp = () => {
+  return (
+    <div class="container">
+      <div class="nav">
+        <a href="/">
+        <img src="https://dummyimage.com/100x50/000/fff" alt="ec_site"/>
+        </a>
+         <ul class="nav_header">
+           <li><a href="https://dummyimage.com/30x30/000/fff">test</a></li>
+           <li><a href="https://dummyimage.com/30x30/000/fff">test</a></li>
+           <li><a href="https://dummyimage.com/30x30/000/fff">test</a></li>
+         </ul>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderComp
