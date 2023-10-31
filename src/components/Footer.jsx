324bd77e36140eb983db640d8142767be86ed670
miox <gallantsummerwind@gmail.com>
2023-10-31 13:23:56 +0900

    FooterCSSの装飾

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa"
 
 export const Footer = () => {
     return (
         <footer>
-            <div className="row">
+            <div>
                 <div>
                     <a href="#"><img src="https://dummyimage.com/30x30/000/fff" alt="miox" /></a>
                     <p>miox,inc.</p>
                 </div>
 
-                <div>
-                    <a href="#"><img src="https://dummyimage.com/30x30/000/fff" alt="Twitter" /></a>
-                    <a href="#"><img src="https://dummyimage.com/30x30/000/fff" alt="Instagram" /></a>
-                    <a href="#"><img src="https://dummyimage.com/30x30/000/fff" alt="Line" /></a>
+                <div class="flex">
+                    <h3 class="text-6xl"> <FaGithub /> </h3>
+                    <h3 class="text-6xl"> <FaGithub /> </h3>
+                    <h3 class="text-6xl"> <FaGithub /> </h3>
+                </div>
+
+                <div class="flex justify-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        お問い合わせ
+                    </button>
                 </div>
 
                 <div>
-                    <a className="btn" href="#" >お問い合わせ</a>
+                    <details>
+                        <summary>
+                            <p>会社情報</p>
+                        </summary>
+                        <li class='footer_link'><a href="#">会社概要</a></li>
+                        <li class='footer_link'><a href="#">製造</a></li>
+                        <li class='footer_link'><a href="#">取材</a></li>
+                    </details>
                 </div>
 
                 <div>
-                    <p>会社情報</p>
-                    <ul className='footer_items'>
-                        <li className='footer_link'><a href="#">会社概要</a></li>
-                        <li className='footer_link'><a href="#">製造</a></li>
-                        <li className='footer_link'><a href="#">取材</a></li>
-                    </ul>
+                    <details>
+                        <summary>
+                            <p>ＩＲ</p>
+                        </summary>
+                        <li class='footer_link'><a href="#">トップ</a></li>
+                        <li class='footer_link'><a href="#">ニュース</a></li>
+                    </details>
                 </div>
+
             </div>
-
             <div>
                 <p class="flex justify-center">miox,inc</p>
             </div>
