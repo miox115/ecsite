fb57212a08b6891e5f152d04a1d8174911d3c45f
miox <gallantsummerwind@gmail.com>
2023-12-30 20:56:32 +0900

    LoginPageの構築

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -5,6 +5,7 @@ import { Home } from '../pages/Home'
 import { Start } from '../pages/Start'
 import { Shop } from '../pages/Shop'
 import { Page404 } from '../pages/Page404'
+import { Login } from '../pages/Login'
 
 export const Router = () => {
     return (
@@ -14,6 +15,7 @@ export const Router = () => {
                     <Route exact path="/" element={<Home />} />
                     <Route path="/Start" element={<Start />} />
                     <Route path="/Shop" element={<Shop />} />
+                    <Route path="/Login" element={<Login />} />
                     <Route path="*" element={<Page404 />} />
                 </Routes>
             </BrowserRouter>
