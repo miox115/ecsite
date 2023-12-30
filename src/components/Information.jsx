d38872940f67b5cbde101444cd6f2cc1e47d2e2d
miox <gallantsummerwind@gmail.com>
2023-12-30 18:27:11 +0900

    一時修正

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Information = () => {
     return (
-        <>
-            <p>周知</p>
+        <div>
+            <p class="flex justify-center">周知</p>
             <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 Button
             </button>
-        </>
+        </div>
     )
 }
