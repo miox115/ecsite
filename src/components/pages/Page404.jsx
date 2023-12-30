77dab0852172666ba9c1ac32dfa85e7ab64e1cd6
miox <gallantsummerwind@gmail.com>
2023-12-30 20:55:57 +0900

    Page404の実装

--- src/components/pages/Page404.jsx
+++ src/components/pages/Page404.jsx
@@ -3,7 +3,33 @@ import React from 'react'
 export const Page404 = () => {
   return (
     <>
-    <p>Page404です。</p>
-    </ >
+      <main className="grid
+        min-h-full 
+        place-items-center 
+        bg-white 
+        px-6 
+        py-24 
+        sm:py-32 
+        lg:px-8">
+
+        <div class="text-center">
+          <p class="text-base
+                  font-semibold
+                  text-indigo-600">
+            404
+          </p>
+
+          <h1
+            class="mt-4
+                text-3xl
+                font-bold
+                tracking-tight
+                text-gray-900
+                sm:text-5xl">
+            Page not found
+          </h1>
+        </div>
+      </main>
+    </>
   )
 }
