ebb7f7ed0cfe8d37259efb33e8ab46fd0cfecdc4
miox <gallantsummerwind@gmail.com>
2023-12-30 20:18:43 +0900

    AtomsのButtonの実装

---
+++ src/components/Atoms/PrimaryButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const PrimaryButton = () => {
+    return (
+        <>
+            <button
+                class="bg-blue-500
+                    hover:bg-blue-700
+                    text-white
+                    font-bold
+                    py-2
+                    px-4
+                    rounded">
+                Button
+            </button>
+        </>
+    )
+}
