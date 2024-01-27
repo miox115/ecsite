680d9d5bc1ba8aec879178dc6461a25ff289909d
NEXT\galla_f3v <gallantsummerwind@gmail.com>
2024-01-27 10:24:32 +0900

    ShopListの商品一覧の修正

--- src/components/Organisms/ShopLayout/ShopList.jsx
+++ src/components/Organisms/ShopLayout/ShopList.jsx
@@ -1,30 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const list = [
-    "食",
-    "菓",
-    "麺",
-    "乾",
-    "詰"
-]
+const lists = ["食", "菓", "麺", "乾", "詰"];
 
 export const ShopList = () => {
-    return (
-        <>
-            <p>商品一覧表</p>
-            <wrapper className="grid grid-cols-2">
-                <a href="#">
-                    <span className='block 
-                bg-slate-200 
-                text-2xl 
-                w-84 
-                h-14 
-                m-4 
-                rounded-md'>
-                        {list}
-                    </span>
-                </a>
-            </wrapper>
-        </>
-    )
-}
+  return (
+    <>
+      <p>商品一覧表</p>
+
+      <wrapper className="grid grid-cols-2">
+        {lists.map((list) => {
+          return (
+            <a href="#">
+              <span
+                className="block 
+    bg-slate-200 
+    text-2xl 
+    w-84 
+    h-14 
+    m-4 
+    rounded-md"
+              >
+                {list}
+              </span>
+            </a>
+          );
+        })}
+      </wrapper>
+    </>
+  );
+};
