60ccb697517f4f4c9c8b833b8f4b867dcd4cc9ab
miox <gallantsummerwind@gmail.com>
2024-02-07 13:05:48 +0900

    もろもろ変更

--- src/components/Organisms/HomeLayout/Support.jsx
+++ src/components/Organisms/HomeLayout/Support.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PrimaryButton } from "../../Atoms/Button/PrimaryButton";
 import { useNavigate } from "react-router-dom";
+import Doctor from "../../../img/img/doctor.jpg";
 
 export const Support = () => {
   const navigate = useNavigate();
@@ -12,7 +13,7 @@ export const Support = () => {
     <div className="text-center">
       <h2>推奨されています</h2>
       <div className="flex justify-center m-2">
-        <img src="https://dummyimage.com/800x200/000/fff" alt="" />
+        <img src={Doctor} alt="Doctor" />
       </div>
       <p>教授推奨</p>
       <p>博士課程終了。各種出版</p>
