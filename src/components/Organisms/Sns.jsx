39e02ecb22e03c08da970948ab05c3ab08adca49
miox <gallantsummerwind@gmail.com>
2024-02-07 11:50:41 +0900

    twitter(react-twitter-embed)の実装

--- src/components/Organisms/Sns.jsx
+++ src/components/Organisms/Sns.jsx
@@ -1,6 +1,38 @@
 import React from "react";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export const Sns = () => {
+  const sns = [
+    {
+      id: 1,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 2,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 3,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 4,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 5,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 6,
+      tweetId: "933354946111705097",
+    },
+    {
+      id: 7,
+      tweetId: "933354946111705097",
+    },
+  ];
+
   return (
     <>
       <div>
@@ -8,89 +40,11 @@ export const Sns = () => {
       </div>
 
       <div className="grid grid-cols-4">
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
-        <div className="m-2">
-          <picture>
-            <img src="https://dummyimage.com/200x/200/fff" alt="goods" />
-          </picture>
-        </div>
+        {sns.map((sns) => (
+          <div key={sns.id} className="m-2">
+            <TwitterTweetEmbed tweetId={sns.tweetId} />
+          </div>
+        ))}
       </div>
     </>
   );
