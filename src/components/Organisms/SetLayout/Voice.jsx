221bd77b6c2ec71b9f3ca40f1fd5a97f78d5c914
miox <gallantsummerwind@gmail.com>
2024-02-14 23:39:05 +0900

    axiosの導入2

--- src/components/Organisms/SetLayout/Voice.jsx
+++ src/components/Organisms/SetLayout/Voice.jsx
@@ -1,130 +1,41 @@
-
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import axios from "axios";
+import { PagiNation } from "../../Atoms/PagiNation/PagiNation";
+import { useEffect, useState } from "react";
 
 export const Voice = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetch = () => {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/posts`)
+        .then((res) => {
+          const slicedData = res.data.slice(0, 10);
+          // とりあえず１０個表示
+          setData(slicedData);
+        })
+        .catch((err) => {
+          console.error(`${err}です。`);
+        });
+    };
+    fetch();
+  }, []);
+
   return (
     <div className="pt-10">
-      <p>データベースとの接続予定</p>
-      <h1>サブタイトル</h1>
-      <h2>タイトル</h2>
-      <p>QtoA</p>
+       <h2 className="text-sm text-amber-300 py-2">口コミ</h2>
+      <h1 className="text-2xl border-b-4 border-yellow-500 pb-2">いろんな意見をいただいています。</h1>
 
-      <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-        <div className="flex flex-1 justify-between sm:hidden">
-          <a
-            href="#"
-            className="relative
-             inline-flex 
-             items-center 
-             rounded-md 
-             border 
-             border-gray-300 
-             bg-white 
-             px-4
-            py-2
-            text-sm
-            font-medium
-            text-gray-700
-            hover:bg-gray-50"
-          >
-            Previous
-          </a>
-          <a
-            href="#"
-            className="relative
-             ml-3 
-             inline-flex 
-             items-center 
-             rounded-md border 
-        border-gray-300 
-        bg-white px-4 
-        py-2 
-        text-sm 
-        font-medium 
-        text-gray-700
-         hover:bg-gray-50"
-          >
-            Next
-          </a>
-        </div>
-
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-          <div>
-            <nav
-              className="isolate inline-flex -space-x-px rounded-md shadow-sm"
-              aria-label="Pagination"
-            >
-              <a
-                href="#"
-                className="relative 
-                inline-flex 
-                items-center
-                 rounded-l-md 
-                 px-2 
-                 py-2
-                  text-gray-400
-                   ring-1 
-                   ring-inset
-                    ring-gray-300
-                     hover:bg-gray-50
-                      focus:z-20 
-                      focus:outline-offset-0"
-              >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
-              {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a
-                href="#"
-                aria-current="page"
-                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                1
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                2
-              </a>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                3
-              </a>
-              <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
-                ...
-              </span>
-              <a
-                href="#"
-                className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-              >
-                8
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                9
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                10
-              </a>
-              <a
-                href="#"
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </a>
-            </nav>
+      {data.map((datas) => {
+        return (
+          <div key={datas.id} className="p-2">
+            <h1 className="p-2">サブタイトル{datas.id}個</h1>
+            <h2 className="p-2">コメント一覧{datas.title}です。</h2>
+            <p className="p-2">内容{datas.body}です。</p>
           </div>
-        </div>
-      </div>
+        );
+      })}
+      <PagiNation />
     </div>
   );
 };
