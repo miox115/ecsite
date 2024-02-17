d1fd6ee3011f9e43c1d11c6ecc84cb378ee25522
miox <gallantsummerwind@gmail.com>
2024-02-17 17:21:43 +0900

    Buttonをクリック時にログインできるように修正

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -64,7 +64,6 @@ export const Login = () => {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  onClick={onClickLogin}
                   onChange={onChangeUserId}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -72,7 +71,7 @@ export const Login = () => {
               </div>
             </div>
           </form>
-          <LoginButton />
+          <LoginButton onClick={onClickLogin} />
         </div>
       </div>
     </>
