a09c3434e69b8b6f48cef22a6d5e9e63ed532921
miox <gallantsummerwind@gmail.com>
2024-02-18 11:54:50 +0900

    軽微な修正

--- src/components/Organisms/CartLayout/LoginProvider.jsx
+++ src/components/Organisms/CartLayout/LoginProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const LoginContext = createContext();
+export const LoginContext = createContext({});
 
 export const LoginProvider = (props) => {
   const [ loginUser, setLoginUser ] = useState(null);
